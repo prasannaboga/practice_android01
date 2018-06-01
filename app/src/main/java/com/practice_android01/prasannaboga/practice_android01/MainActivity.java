314aeb5ecaedc1d5b3cfcb4492e85bb492e28ad7
prasanna.boga <prasannaboga@gmail.com>
2018-06-01 14:03:26 +0530

    Started drawer layout

--- app/src/main/java/com/practice_android01/prasannaboga/practice_android01/MainActivity.java
+++ app/src/main/java/com/practice_android01/prasannaboga/practice_android01/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
+        setContentView(R.layout.activity_main);
     }
 
     public void toastMe(View view) {
